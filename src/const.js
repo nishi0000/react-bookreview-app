23a42469d860ba23068101bfaa537874b0eb03bf
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2023-10-13 18:26:03 +0900

    Delete src directory

--- src/const.js
+++
@@ -1,6 +0,0 @@
-export const url = process.env.REACT_APP_API_URL;
-
-// ルートディレクトリとは「これ以上は上に行けないよ」なディレクトリ
-// 環境変数とはコンピュータが持っている変数のこと
-// envはprocess.env.(変数名)で呼び出せる
-// つまり、urlに入ってるのは"https://qvg1o8w2ef.execute-api.ap-northeast-1.amazonaws.com/"てことか
--- src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authSlice } from "./features/AuthSlice";
-
-export const store = configureStore({
-    reducer: {
-      auth: authSlice.reducer,
-    },
-  });
